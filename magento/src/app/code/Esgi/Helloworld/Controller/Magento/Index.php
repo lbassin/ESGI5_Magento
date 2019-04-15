6fc1decf2ced441efdcbd0af6caae1cd9726898a
Laurent Bassin <laurent@bassin.info>
2019-04-15 16:34:32 +0200

    Add PageFactory

--- magento/src/app/code/Esgi/Helloworld/Controller/Magento/Index.php
+++ magento/src/app/code/Esgi/Helloworld/Controller/Magento/Index.php
@@ -5,12 +5,26 @@ declare(strict_types=1);
 namespace Esgi\Helloworld\Controller\Magento;
 
 use Magento\Framework\App\Action\Action;
-use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
 
 class Index extends Action
 {
-    public function execute(): ResponseInterface
+    protected $resultPageFactory;
+
+    public function __construct(Context $context, PageFactory $resultPageFactory)
     {
-        die('Magento_Index');
+        $this->resultPageFactory = $resultPageFactory;
+
+        parent::__construct($context);
+    }
+
+    public function execute(): ResultInterface
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->set(__('Helloworld Magento Index'));
+
+        return $resultPage;
     }
 }
