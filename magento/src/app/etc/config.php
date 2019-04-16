22dc6e9830ba6559794a495b843e9c804507976d
Laurent Bassin <laurent@bassin.info>
2019-04-16 10:16:32 +0200

    Add setup

--- magento/src/app/etc/config.php
+++ magento/src/app/etc/config.php
@@ -6,6 +6,7 @@ return [
         'Magento_Theme' => 1,
         'Magento_Backend' => 1,
         'Esgi_Helloworld' => 1,
+        'Esgi_Job' => 1,
         'Magento_Variable' => 1,
         'Magento_Eav' => 1,
         'Magento_AdminNotification' => 1,
